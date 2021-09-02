27fa69f4dcf8cb89cff44642c09d9cdf2b355d7a
Gaurav Sharma <sgaurav81@gmail.com>
2021-09-02 19:55:31 +0530

    comment -1

--- src/main/java/com/gaurav/base/BaseProjectApplication.java
+++ src/main/java/com/gaurav/base/BaseProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BaseProjectApplication {
 
+	/** Added Comment -1 **/
 	public static void main(String[] args) {
 		SpringApplication.run(BaseProjectApplication.class, args);
 	}
